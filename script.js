d453d8e33542c74ce909dced3e2dafd8fba3cb11
Renan Lemos <renanlemos1612@gmail.com>
2023-12-10 02:19:58 -0300

    Finalizado script do botão dark e light mode

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 function toggleMode() {
   const html = document.documentElement;
-
+  html.classList.toggle("light");
+  // ou de forma mais completa usando o if:
   // if(html.classList.contains('light')) {
   //   html.classList.remove('light');
   // }else {
   //   html.classList.add('light');
-  // } ou de forma resumida:
-  html.classList.toggle('light');
+  // }
+
+  // pegar a tag img.
+  const img = document.querySelector("#profile img");
+  //  substituir a imagem.
+  if (html.classList.contains("light")) {
+    // Se tiver no light mode, adicionar a imagem light.
+    img.setAttribute("src", "images/avatar-light.png");
+    img.setAttribute(
+      "alt",
+      "foto de renan em uma praça usando óculos de grau e barba"
+    );
+  } else {
+    //  Se tiver no dark mode, manter a imagem original.
+    img.setAttribute("src", "images/avatar.png");
+  }
 }
